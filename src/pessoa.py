26f4de872b528a43475ee6a8c2c96f8c9e9dc4a6
italodellagarza <italodellagarza@hotmail.com>
2019-05-03 14:55:04 -0300

    Comecar classe person.py

---
+++ src/pessoa.py
@@ -0,0 +1,15 @@
+class Person:
+    
+    
+    def __init__(self, name, image_dir):
+        self.name = name
+        self.image_dir = image_dir
+        
+    def name(self):
+        return self.name
+    
+    def image_dir(self)
+        return self.image_dir
+        
+    def load_images(self)
+        
