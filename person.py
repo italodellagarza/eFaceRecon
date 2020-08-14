dd0451739a43dc319fff61b8777a85eb0abb477e
italodellagarza <italodellagarza@hotmail.com>
2020-08-14 10:15:27 -0300

    Resultados Iniciais

--- person.py
+++ person.py
@@ -4,18 +4,21 @@ import numpy as np
 class Person:
     
     
-    def __init__(self, base, name, file):
+    def __init__(self, base, name, file, vp_exists=""):
         self.name = name
         self.base = base
         self.file = file
         self.embedded_nn4 = np.zeros(128)
         self.embedded_facenet = np.zeros(128)
         self.embedded_vgg = np.zeros(2622)
+        self.vp_exists = vp_exists
     
     def __repr__(self):
         return self.image_path()
 
     def image_path(self):
+        if self.vp_exists != "" :
+            return os.path.join(self.base, self.name, self.vp_exists, self.file)
         return os.path.join(self.base, self.name, self.file)
 
     def load_embedded(self, neural_network):
